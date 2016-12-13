99c07e411cdbee1d9aeb4be503d17ab3367d531a
Adrian Avendano <adriano@Adrians-MacBook-Pro-2.local>
2016-12-13 16:49:33 +0800

    adding database

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 var fs = require("fs");
+//var debug = require('debug')
+var mongo = require('mongodb');
+var monk = require('monk');
+var db2 = monk('54.197.18.144:27017/globalsouth');
+//var db = monk('amonter5:espana6248@54.175.50.182:27017/globalsouth');
 var mandrill = require('mandrill-api/mandrill');
 var mandrill_client = new mandrill.Mandrill('NHpudxhV9HV6zakj7-gH0A');
 var readline = require('linebyline'),
-      rl = readline('partners.csv');
+      rl = readline('edtech_list.csv');
 
 var count = 0;
 var la;
+
+//checkEmail();
+connectMongo();
+
+
 rl.on('line', function(line, lineCount, byteCount) {
 
 	
@@ -15,21 +25,23 @@ rl.on('line', function(line, lineCount, byteCount) {
 	//la+=", "+language;	
 	if (lineCount > 0){		
 		var theName = elements[0].match(/\S+/g);		
-		//console.log(theName[0] +" "+ elements[3]);replace(/"/g, "")
-		if (elements[3] == 'general update'){			
-			
-			if (elements[5] == "NO"){
-				var subSet = new String(elements[4]);													
-				//if (lineCount == 5){
-					sendMail(theName[0], elements[1], subSet.replace(/"/g, ""), elements[6].trim());
+		//console.log(elements[0]+" "+elements[1]+" "+elements[2]);
+		var theEmail = elements[1];
+		var company = theEmail.split('@');
+		//console.log(company[1]);
+		//if (elements[5] == 'yes'){			
+			//console.log(elements[0] +" ("+ elements[2]+")<br>");	
+				//var subSet = new String(elements[4]);													
+				//if (lineCount == 3){
+					//console.log(elements[0] +" - - "+ elements[1]);
+					//sendMail(theName[0], elements[1], "", "");
 				//}
-			} else {
 				
-			}
+		//}
 
 			//console.log(elements[0]+"\n"+elements[4].replace(""", ""));			
-		}		
-	}
+	}		
+	
 
 	/*if (elements[1].length > 0 && elements[3] == ‘Comments’){
 		//for ( var i = 0; i < elements.length;i++){
@@ -57,10 +69,22 @@ rl.on('end', function () {
 	//console.log("END "+la);
 });
 
+function connectMongo(){
+	var collection = db2.get('people');
+	collection.find({}, function (err, doc) {
+		console.log(doc[0]);
+	});
+}
+
+function checkEmail(){
+	//web-yPcyzu@mail-tester.com
+	sendMail('Petra', 'web-7NLcV8@mail-tester.com', "", "");
+}
+
 function sendMail (name, email, update, followup) {
 
 console.log(name+" "+email+" \n "+ update+" \n ");
-var template_name = "catch-two-custom";
+var template_name = "catch-up";
 var template_content = [{
         "name": "example name",
         "content": "example content"
@@ -77,9 +101,7 @@ var message = {
 			"rcpt": email,
 			"vars": [{
 				"name": "firstname",
-				"content": name},
-				{"name": "part_one",
-				"content": update}
+				"content": name}
 			]}],
 		    "from_name": "Adrian Avendano",
 		    "to": [{
